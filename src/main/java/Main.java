14bf88d94736b8e4e6b7b822882f78dfd3e1c499
Sergey Andreev <serzh28andreev.com>
2025-04-04 09:28:37 +0300

    class Car, Race разделены по файлам

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,39 +6,39 @@ public class Main {
         Race race = new Race();
         Car[] cars = new Car[3];
 
-        for (int i = 0; i < cars.length; i++) {
-            System.out.print("Введите название автомобиля " + (i + 1) + ": ");
-            String carName = scanner.nextLine();
-            int speed;
-
-
-            while (true) {
-                System.out.print("Введите скорость автомобиля (0-250): ");
-                speed = scanner.nextInt();
-
-                if (speed >= 0 && speed <= 250) {
-                    break;
-                } else {
-                    System.out.println("Ошибка: скорость должна быть от 0 до 250. Попробуйте снова.");
+            for (int i = 0; i < cars.length; i++) {
+                System.out.print("Введите название автомобиля №" + (i + 1) + ": ");
+                String carName = scanner.nextLine();
+                if (carName.trim().isEmpty()) {
+                    System.out.println("Ошибка: строка не должна быть пустой. Попробуйте снова.");
                 }
+                int speed;
+
+                while (true) {
+                    System.out.print("Введите скорость автомобиля (0-250): ");
+
+                    try {
+                        speed = scanner.nextInt();
+                        if (speed >= 0 && speed <= 250) {
+                            break;
+                        } else {
+                            System.out.println("Ошибка: скорость должна быть от 0 до 250. Попробуйте снова.");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Ошибка, введите числовое значение.");
+                        scanner.next();
+                    }
+                }
+                scanner.nextLine();
+
+
+                cars[i] = new Car(carName, speed);
+
+                race.determineLeader(cars[i]);
             }
-            scanner.nextLine();
 
 
-            cars[i] = new Car(carName, speed);
-
-            race.determineLeader(cars[i]);
+            System.out.println("Победитель гонки: " + race.getCurrentLeader());
+            scanner.close();
         }
-
-
-        System.out.println("Победитель гонки: " + race.getCurrentLeader());
-        scanner.close();
     }
-
-}
-
-
-
-
-
-
